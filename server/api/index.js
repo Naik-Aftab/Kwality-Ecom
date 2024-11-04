eb5544f5520bc94b00385d74744e1a5bbda8c8a5
naik-aftab <aftabnaik1999@gmail.com>
2024-11-04 19:17:40 +0530

    added api in index.js

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,9 @@ const categoryRoutes = require('../routes/category');
 const productRoutes = require('../routes/product');
 const orderRoutes = require('../routes/order');
 const customerRoutes = require('../routes/customer');
-const metricsRoutes = require('../routes/metrics'); 
+const metricsRoutes = require('../routes/metrics');
+const porterRoutes = require('../routes/porter'); 
+const loggerMiddleware = require('../middlewares/logger');
 
 dotenv.config();
 connectDB();
@@ -18,20 +20,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Apply logger middleware
+app.use(loggerMiddleware);
+
 // Serve uploaded images as static files
 app.use("/uploads", express.static("public/uploads"));
 
 
 // Routes
 app.use('/auth', authRoutes);           // Auth routes
-app.use('/categories', categoryRoutes); // Category routes
-app.use('/products', productRoutes);    // Product routes
+app.use('/api/categories', categoryRoutes); // Category routes
+app.use('/api/products', productRoutes);    // Product routes
 app.use('/orders', orderRoutes);        // Order routes
 app.use('/customers', customerRoutes);  // Customers
 app.use('/metrics', metricsRoutes);     // Metrics routes
+app.use('/porter', porterRoutes);     // Metrics routes
 
 
-// Listen on port 5000
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
